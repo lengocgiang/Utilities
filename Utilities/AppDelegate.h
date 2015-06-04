d4509e698ee8c87f2d3d612b71ebd597a043e08b
GiangLeNgoc <giangln@Giangs-MacBook-Pro.local>
2015-06-04 22:14:28 +0700

    Initial Commit

---
+++ Utilities/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Utilities
+//
+//  Created by GiangLeNgoc on 6/4/15.
+//  Copyright (c) 2015 GiangLeNgoc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Utilities/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Utilities
+//
+//  Created by GiangLeNgoc on 6/4/15.
+//  Copyright (c) 2015 GiangLeNgoc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
